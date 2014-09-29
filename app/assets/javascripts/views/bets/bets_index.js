e2f7bd7cc98c8193743ab730c693c05d056271ad
App Academy Student <student@appacademy.io>
2014-09-29 10:49:16 -0700

    remove bets from ticket

--- app/assets/javascripts/views/bets/bets_index.js
+++ app/assets/javascripts/views/bets/bets_index.js
@@ -1,27 +1,3 @@
-// Superbook.Views.BetsIndex = Backbone.CompositeView.extend({
-//   template: JST["bets/index"],
-//
-//   initialize: function () {
-//     // this.listenTo(this.collection, 'add sync', this.render);
-//     this.listenTo(this.collection, 'sync', this.render);
-//     // new method to add subview for new bets
-//     this.listenTo(this.collection, 'add', this.addNewBet)
-//   },
-//
-//   events: {
-//
-//   },
-//
-//   render: function () {
-//     var renderedContent = this.template({
-//       bets: this.collection
-//     });
-//
-//     this.$el.html(renderedContent);
-//     return this;
-//   }
-// });
-
 Superbook.Views.BetsIndex = Backbone.CompositeView.extend({
   template: JST["bets/index"],
   
@@ -29,22 +5,8 @@ Superbook.Views.BetsIndex = Backbone.CompositeView.extend({
     // this.listenTo(this.collection, 'add sync', this.render);
     this.listenTo(this.collection, 'sync', this.render);
     // new method to add subview for new bets
-    this.listenTo(this.collection, 'add', this.addNewBet)
+    this.listenTo(this.collection, 'add', this.addNewBet);
   },
-  
-  events: {
-		"click .remove-bet": "removeBet",
-		"submit": "submitBet"
-  },
-	
-	submitBet: function (event) {
-		event.preventDefault();
-		debugger
-	},
-	
-	removeBet: function (event) {
-		debugger
-	},
 	
 	addNewBet: function () {
 		// add subview for new bet 
@@ -58,9 +20,7 @@ Superbook.Views.BetsIndex = Backbone.CompositeView.extend({
 	},
   
   render: function () {
-    var renderedContent = this.template({
-      bets: this.collection
-    });
+    var renderedContent = this.template();
 
     this.$el.html(renderedContent);
 	  this.attachSubviews();
--- app/assets/javascripts/views/bets/bets_new.js
+++ app/assets/javascripts/views/bets/bets_new.js
@@ -27,6 +27,25 @@ Superbook.Views.BetNew = Backbone.CompositeView.extend({
     this.listenTo(this.model, 'sync', this.render);
   },
   
+  events: {
+    "submit": "submitBet",
+    "click .remove-bet": "removeBet"
+  },
+  
+	submitBet: function (event) {
+		event.preventDefault();
+
+    debugger
+	},
+	
+	removeBet: function (event) {
+    // remove subview selector: '#bets-list subview: IDK
+    this.removeSubview("#bets-list", this);
+    Superbook.Collections.bets.remove(this.model);
+    // find button and remove active class
+    $(".btn.team." + this.model.attributes.team.id).removeClass("active");
+	},
+  
   render: function () {
 		var renderedContent = this.template({ bet: this.model });
     this.$el.html(renderedContent);
