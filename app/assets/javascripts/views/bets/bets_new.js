e763740e9857a1c8559726274e4e083fec90ce1b
App Academy Student <student@appacademy.io>
2014-09-30 14:25:52 -0700

    styling

--- app/assets/javascripts/views/bets/bets_new.js
+++ app/assets/javascripts/views/bets/bets_new.js
@@ -1,31 +1,8 @@
-// Superbook.Views.BetNew = Backbone.CompositeView.extend({
-//   template: JST["bets/new"],
-//   promptTemplate: JST["bets/prompt"],
-//   initialize: function () {
-//     this.listenTo(this.model, 'sync bet-started', this.render);
-//   },
-//
-//   render: function () {
-//     if(this.model.team && this.model.game){
-//       var content = this.template({
-//         game: this.model
-//       });
-//     } else {
-//       var content = this.promptTemplate();
-//     }
-//     this.$el.html(content);
-//     return this;
-//   }
-//
-//
-// });
-
 Superbook.Views.BetNew = Backbone.CompositeView.extend({
   template: JST["bets/new"],
 
   initialize: function () {
     this.listenTo(this.model, 'sync', this.render);
-
   },
   
   events: {
@@ -69,9 +46,6 @@ Superbook.Views.BetNew = Backbone.CompositeView.extend({
     
     newBet.save({}, {
       success: function () {
-        // add to some collection?
-        // remove subview
-        // remove class active
         that.removeSubview("#bets-list", that);
         $(".btn.team." + that.model.attributes.team.id).removeClass("active");
         
--- app/controllers/api/bets_controller.rb
+++ app/controllers/api/bets_controller.rb
@@ -2,7 +2,6 @@ module Api
   class BetsController < ApiController
     def create
       @bet = current_user.bets.new(self.bet_params)
-      # to_win(@bet, @bet.game.odds)
       @bet.result = "pending"
     
       if @bet.save
@@ -13,16 +12,12 @@ module Api
         render json: @bet.errors, status: :unprocessable_entity
       end
     end
+    
+    def update
+      
+    end
 
     protected
-
-    # def to_win(bet, odds)
-    #   if bet.team == bet.game.team1
-    #     bet.to_win = odds / (1 - odds) * bet.amount
-    #   else
-    #     bet.to_win = (1 - odds) / odds * bet.amount
-    #   end
-    # end
 
     def bet_params
       self.params[:bet].permit(:amount, :team_id, :game_id, :to_win)
